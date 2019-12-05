9badc9820705297e945a9e6030c3972c156b8f26
Robert Flaugher <rlfprog@gmail.com>
2019-12-05 15:06:31 -0800

    Make hamburger button work

--- start/ch03_coding_the_navigation/js/menuToggle.js
+++ start/ch03_coding_the_navigation/js/menuToggle.js
@@ -1,7 +1,10 @@
 //Create a button and give it an ID of "hamburgerBtn"
 //Attach an ID of "primaryNav" to the UL of the unordered list that holds your links
 function toggleMenu() {
+    // Add class "closed" to ul element if it's missing and remove it if it's there.
     document.getElementById("primaryNav").classList.toggle("closed");
 }
-var x = document.getElementById('hamburgerBtn');
-x.onclick = toggleMenu; 
+// Creating this "x" allows you to put logic here instead of assigning an onclick event to the button HTML element.
+// When the hamburger button is clicked, the toggleMenu function will run.
+var btn = document.getElementById('hamburgerBtn');
+btn.onclick = toggleMenu;
